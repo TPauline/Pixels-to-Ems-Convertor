a66c1a461907cf2e4870422674de67486f9417ab
TPauline <73195686+TPauline@users.noreply.github.com>
2022-12-10 11:07:02 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 const wrapper = document.getElementById('wrapper');
-const topArea = document.getElementById('topArea');
-const middleArea = document.getElementById('middleArea');
-const bottomArea = document.getElementById('bottomArea');
-const plusDiv = document.getElementById('plusDiv');
-const plusLogo = document.getElementById('plusLogo');
+const topArea = document.getElementById('mainArea');
+const defaultInput = document.getElementById('default');
+const convertPX = document.getElementById('convertPX');
+const convertEM = document.getElementById('convertEM');
+const calResult = document.getElementById('result');
+
 // const letters = new Set(["a","b","c"]);
 
+
+// window.getComputedStyle(document.body) - to get all the styles for body
+// getPropertyValue('font-size') - to get a string value of font-size, example: (16px)
+// match(/\d+/)[0]) - to get only a number part, example: (16) - string
+// Number(...) - to convert number part into a number, example: (16) - number
+//const fontSize = Number(window.getComputedStyle(document.body).getPropertyValue('font-size').match(/\d+/)[0])
+const fontSize = Number(window.getComputedStyle(document.body).getPropertyValue('font-size').match(/\d+/)[0])
+let defaultSize = null
+
+console.log(fontSize)
 console.log(window);
 console.log(document);
 console.log(window.innerWidth);
 console.log(wrapper.style.width);
 console.log(wrapper.style);
-
-topList = [];
-tasksList = [];
-favoriteTasksList = [];
-
-
-
 
 function setUp() {
 
@@ -31,26 +35,142 @@ function setUp() {
 }
 setUp();
 
+function enableDisable(node) {
+    if (node.disabled) {
+        node.disabled = false;
+    } else {
+        node.disabled = true;
+    }
+}
+
+// convertPX.disabled = true
+// convertEM.disabled = true
+enableDisable(convertPX)
+enableDisable(convertEM)
+
+
+
 function piexl_To_em(R, P) {
     /*Where,
         R = Required PX Size
         P = Parent PX Size
     */
+    // const EM = R / P;
+    if(!isNaN(P)){
     const EM = R / P;
-    return EM
+    console.log(`converted to em is ${EM}em`)
+    return EM}
 }
 
 
+function em_To_piexl(EM, P) {
+    /*Where,
+        R = Required PX Size
+        P = Parent PX Size
+    */
+    // const EM = R / P;
+    if(!isNaN(P)){
+    const R = EM * P;
+    console.log(`converted to em is ${R}px`)
+    return EM
+    }
+}
 
-
-
-
+piexl_To_em(16, fontSize)
+em_To_piexl(16, fontSize)
 /**Event Listeningers */
 window.addEventListener("resize", () => {
-    document.body.clientWidth = window.innerWidth;
-    document.body.clientHeight = window.innerHeight;
-    console.log("new body dimensions: (", document.body.clientHeight, ",", document.body.clientWidth, ")");
-    wrapper.style.width = document.body.clientWidth;
-    wrapper.style.height = document.body.clientHeight;
+    // document.body.clientWidth = window.innerWidth;
+    // document.body.clientHeight = window.innerHeight;
+    // console.log("new body dimensions: (", document.body.clientHeight, ",", document.body.clientWidth, ")");
+    // wrapper.style.width = document.body.clientWidth;
+    // wrapper.style.height = document.body.clientHeight;
+    setUp()
 
 })
+
+defaultInput.addEventListener("change", () => {
+    console.log("enter default");
+
+    convertPX.disabled = false
+    convertEM.disabled = true
+});
+
+convertPX.addEventListener("click", () => {
+    console.log("enter px");
+
+    convertPX.disabled = false
+    convertEM.disabled = true
+});
+
+convertEM.addEventListener("click", () => {
+    console.log("enter em");
+
+    convertPX.disabled = true
+    convertEM.disabled = false
+});
+
+
+// document.addEventListener('click',  (e) => {
+//     console.log(e.target)
+//     target =e.target;
+//     if( e.target.id == "convertPX" ){
+//         // convertPX.disabled = true
+//         enableDisable(e.target);
+//     }
+// });
+
+// document.addEventListener('mouseup',  (e) => {
+//     let target = e.target
+//     if( target == convertPX || target == convertEM){
+//         enableDisable(target)
+//         enableDisable(target)
+//     }
+// });
+
+document.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target == convertPX) {
+        convertPX.disabled = false;
+        convertEM.disabled = true;
+    } else if (target == convertEM) {
+        convertPX.disabled = true;
+        convertEM.disabled = false;
+    }
+});
+
+document.addEventListener('input', (e) => {
+    let target = e.target;
+    console.log("**", target, target.value)
+    if (target == defaultInput) {
+        if (!isNaN(target.value)) {
+            defaultSize = target.value
+            if (!isNaN(convertPX.textContent)) {
+                let px = em_To_piexl(target.value, defaultSize)
+                calResult.textContent = `Result: ${px}em`;
+            } else if (!isNaN(convertEM.textContent)) {
+                let em = piexl_To_em(target.value, defaultSize)
+                calResult.textContent = `Result: ${em}em`;
+            }else{
+                calResult.textContent = `Result: `;
+            }
+        }
+    } else if (target == convertPX) {
+        convertEM.value = ""
+        if (!isNaN(target.value) && !isNaN(defaultInput.value) && !isNaN(defaultSize)) {
+            let em = piexl_To_em(target.value, defaultSize)
+            calResult.textContent = `Result: ${em}em`;
+        } else {
+            calResult.textContent = `Result: 0px`;
+        }
+    } else if (target == convertEM) {
+        convertPX.value = ""
+        if (!isNaN(target.value) && !isNaN(defaultInput.value) && !isNaN(defaultSize)) {
+            let px = em_To_piexl(target.value, defaultSize)
+            calResult.textContent = `Result: ${px}em`;
+        } else {
+            calResult.textContent = `Result: 0em`;
+        }
+
+    }
+});
